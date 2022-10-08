58f59608f63609484be7c1bdb87993e334ed88b1
Rajesh Majumdar <rajeshmjmdr02@gmail.com>
2022-10-08 15:34:07 +0530

    Switched to PM.

--- README.md
+++ README.md
@@ -10,12 +10,14 @@ As I keep on learning, I will keep updating the README page with necessary code.
 - [Vim](https://www.vim.org/)
 
 ### Progress
-- [x] Lauched boot sequence
+- [x] Lauched from boot sector.
 - [x] Switched to TTY mode and printed some characters.
 - [x] Loops and functions.
 - [x] Learned about segmentations.
-- [x] Implementing stack
-- [ ] Switching from Real to Protected mode.
+- [x] Implementing stack.
+- [x] Switching from Real to Protected mode.
+- [x] Print a welcome message in PM.
+- [ ] Finally jumping to kernel.
 
 ### Resources
 - [osdev.wiki](https://wiki.osdev.org)
--- boot.asm
+++ boot.asm
@@ -1,29 +1,69 @@
-mov ah, 0x0e
+[org 0x7c00]
 
-mov bp, 0x8000
-mov sp, bp
+CODE_SEG    equ     code_descriptor - GDT_Start
+DATA_SEG    equ     data_descriptor - GDT_Start
+VIDEO_MEMORY    equ 0xb8000
+CHAR_OFFSET     equ 0x00002
 
-push 'A'
-push 'B'
-push 'C'
+cli     ; disable all interrupts
+lgdt [GDT_Descriptor]
 
-mov al, [0x7ffe]
-int 0x10
+mov eax, cr0
+or  eax, 1
+mov cr0, eax    ; now we are in 32 bit mode
 
-pop bx
-mov al, bl
-int 0x10
+jmp CODE_SEG:start_protected_mode
+jmp $
 
-pop bx
-mov al, bl
-int 0x10
+GDT_Start:
+    null_descriptor:
+        dd  0
+        dd  0
+    code_descriptor:
+        dw  0xffff  ; limit of our segment
+        dw  0       ; 16 bits +
+        db  0       ; 8 bits = 24 our base of the segment
+        db  0b10011010 ; present, privilege, type and type flags
+        db  0b11001111 ; other flags + limit
+        db  0       ; last 8 bits of the base
+    data_descriptor:
+        dw  0xffff
+        dw  0
+        db  0
+        db  0b10010010 ; Same as code descriptor, but last 4 are changed type flags
+        db  0b11001111
+        db  0
+GDT_End:
 
-pop bx
-mov al, bl
-int 0x10
+GDT_Descriptor:
+    dw  GDT_End - GDT_Start - 1 ; size of the GDT
+    dd  GDT_Start               ; start of the GDT
 
-mov al, [0x8000]
-int 0x10
+[bits 32]
+start_protected_mode:
+    jmp startOS
+
+welcomeMessage:
+    db  "Welcome to petOS - n0xne", 0
+
+startOS:
+    mov bx, welcomeMessage
+    mov ecx, 0
+    jmp printMessage
+
+printMessage:
+    mov eax, CHAR_OFFSET
+    mul ecx
+    mov ah, 0x0f
+    mov al, [bx]
+    cmp al, 0
+    je  endMessage
+    mov [VIDEO_MEMORY + (ecx * CHAR_OFFSET)], ax
+    inc cx
+    inc bx
+    jmp printMessage
+
+endMessage:
 
 jmp $
 
